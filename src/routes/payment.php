78418d12bb1c4e18eb8b1c26eb951ecd67449da7
Sashagm <example@example.com>
2022-10-15 21:38:19 +0500

    fix namespace routes

--- src/routes/payment.php
+++ src/routes/payment.php
@@ -1,24 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Sashagm\Payment\Http\Controllers\PaymentPayeerController;
 
 Route::group(['middleware' => ['web']], function () {
    
 /* Главная */
-Route::get('/payment', [\Sashagm\Payment\Http\Controllers\PaymentController::class, 'index'])->name('paymentIndex');
+Route::get('/payment', [PaymentController::class, 'index'])->name('paymentIndex');
 
 /* Статусы платежей */
-Route::get('/payment/success', [\Sashagm\Payment\Http\Controllers\PaymentController::class, 'success'])->name('paymentSuccess');
-Route::get('/payment/fail', [\Sashagm\Payment\Http\Controllers\PaymentController::class, 'fail'])->name('paymentFail');
+Route::get('/payment/success', [PaymentController::class, 'success'])->name('paymentSuccess');
+Route::get('/payment/fail', [PaymentController::class, 'fail'])->name('paymentFail');
 
 /* Обработка форм */
-Route::post('/payment/freekassa_form', [\Sashagm\Payment\Http\Controllers\PaymentFreekassaController::class, 'freekassaForm'])->name('freekassaForm');
-Route::post('/payment/payeer_form', [\Sashagm\Payment\Http\Controllers\PaymentPayeerController::class, 'payeerForm'])->name('payeerForm');
-Route::post('/payment/webmoney_form', [\Sashagm\Payment\Http\Controllers\PaymentWebmoneyController::class, 'webmoneyForm'])->name('webmoneyForm');
+Route::post('/payment/freekassa_form', [PaymentFreekassaController::class, 'freekassaForm'])->name('freekassaForm');
+Route::post('/payment/payeer_form', [PaymentPayeerController::class, 'payeerForm'])->name('payeerForm');
+Route::post('/payment/webmoney_form', [PaymentWebmoneyController::class, 'webmoneyForm'])->name('webmoneyForm');
 
 /* Получение ответа */
-Route::post('/payment/freekassa', [\Sashagm\Payment\Http\Controllers\PaymentFreekassaController::class, 'freekassa'])->name('freekassa');
-Route::post('/payment/payeer', [\Sashagm\Payment\Http\Controllers\PaymentPayeerController::class, 'payeer'])->name('payeer');
+Route::post('/payment/freekassa', [PaymentFreekassaController::class, 'freekassa'])->name('freekassa');
+Route::post('/payment/payeer', [PaymentPayeerController::class, 'payeer'])->name('payeer');
 
 
 
